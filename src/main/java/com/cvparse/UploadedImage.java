e4fcb8d6af27413f8f9088acd71bca5debbea5b0
mikebochenek <mike@bochenek.ca>
2013-03-22 23:03:24 +0100

    file upload with rich faces

---
+++ src/main/java/com/cvparse/UploadedImage.java
@@ -0,0 +1,27 @@
+package com.cvparse;
+
+public class UploadedImage {
+
+	private byte[] data;
+	private String name;
+	private long length;
+	
+	public byte[] getData() {
+		return data;
+	}
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getLength() {
+		return length;
+	}
+	public void setLength(long length) {
+		this.length = length;
+	}
+}
