20a2f87eed3dd1ee3a020e615e42a270855f189c
mikebochenek <mike@bochenek.ca>
2013-06-25 00:02:05 +0200

    adding my very first junit

---
+++ src/test/java/com/cvparse/pojo/ProblemDefinitionTest.java
@@ -0,0 +1,14 @@
+package com.cvparse.pojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProblemDefinitionTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
