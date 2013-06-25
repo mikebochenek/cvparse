9706d635d8d36387789660fcff885be2d56774d7
mikebochenek <mike@bochenek.ca>
2013-06-25 07:01:19 +0200

    implement real test

--- src/test/java/com/cvparse/pojo/ProblemDefinitionTest.java
+++ src/test/java/com/cvparse/pojo/ProblemDefinitionTest.java
@@ -8,7 +8,19 @@ public class ProblemDefinitionTest {
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
+		ProblemDefinition def = new ProblemDefinition();
+		
+		Function f = new Function();
+		f.setName("Factorial");
+		Variable ret = new Variable();
+		ret.setType(VariableType.INTEGER);
+		f.setReturnValue(ret);
+		
+		Variable param = new Variable();
+		param.setType(VariableType.INTEGER);
+		f.addParameter(param);
+		
+		def.setTestFunction(f);
 	}
 
 }
