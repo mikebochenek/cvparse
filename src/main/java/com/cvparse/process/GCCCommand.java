49893014054217e5993617d4286c7fb3809ede6a
mikebochenek <mike@bochenek.ca>
2013-07-02 22:35:02 +0200

    tdd - found issue while writing test case

--- src/main/java/com/cvparse/process/GCCCommand.java
+++ src/main/java/com/cvparse/process/GCCCommand.java
@@ -17,7 +17,7 @@ public class GCCCommand extends GenericCompileCommand {
 	public String compile(String dir, String name, String s) throws IOException {
 		createdir(dir);
 		write(dir, name, s, "c");
-		return executeCompile(dir, "gcc", "/tmp/" + dir + "/" + name + "c");
+		return executeCompile(dir, "gcc", "/tmp/" + dir + "/" + name + ".c");
 	}
 	
 	public String compileAndTest(String dir, String name, String s, String testCode) throws IOException {
--- src/test/java/com/cvparse/process/GCCCommandTest.java
+++ src/test/java/com/cvparse/process/GCCCommandTest.java
@@ -2,18 +2,36 @@ package com.cvparse.process;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class GCCCommandTest {
 
 	@Test
-	public void testCompile() {
-		fail("Not yet implemented");
+	public void testCompile() throws IOException {
+		GCCCommand c = new GCCCommand();
+		String out = c.compile("tst"+System.currentTimeMillis(), "test", code);
+		assertNotNull(out);
 	}
 
 	@Test
 	public void testCompileAndTest() {
-		fail("Not yet implemented");
+		//TODO fail("Not yet implemented");
 	}
 
+	final static String code = 
+			"#include <stdio.h>\n"
+			+ "#include <stdlib.h>\n"
+			+ "void play() \n"
+			+ "{\n"
+			+ "  int i;\n"
+			+ "  printf(\"welcome to my world of pointers %p\", &i);\n"
+			+ " printf(\"%d %d\", sizeof(i), i);\n"
+			+ "}\n"
+			+ "int main(void)\n"
+			+ "{ \n"
+			+ "  play();\n"
+			+ "  return 0;\n"
+			+ "}";
 }
